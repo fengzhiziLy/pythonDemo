d3ff21d32ee165a114a4d8119ae5803e7326c31b
feng <Ly332883899@163.com>
2020-06-10 17:46:19 +0800

    使用cookie实现登录

--- test_selenium/test_wework.py
+++ test_selenium/test_wework.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions
@@ -8,9 +10,10 @@ class TestSelenium:
     def setup(self):
         # 连接调试开关打开的Chrome进程
         # 通过这种方式可以绕过企业微信的登录
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.debugger_address = "127.0.0.1:9222"
-        self.driver = webdriver.Chrome(options=chrome_options)
+        # chrome_options = webdriver.ChromeOptions()
+        # chrome_options.debugger_address = "127.0.0.1:9222"
+        # self.driver = webdriver.Chrome(options=chrome_options)
+        self.driver = webdriver.Chrome()
         # 隐式等待
         self.driver.implicitly_wait(5)
 
@@ -32,3 +35,24 @@ class TestSelenium:
         )
         self.driver.execute_script("arguments[0].click();",
                                    self.driver.find_element_by_css_selector(".js_next"))
+
+    # 使用cookie登录
+    def test_cookie(self):
+        url = "https://work.weixin.qq.com/wework_admin/frame#contacts"
+        self.driver.get(url)
+        # 使用无痕模式浏览，比较两次请求的cookie的不同
+        cookies = {
+            "wwrtx.d2st": "a5373133",
+            "wwrtx.sid": "rX_NDIktUTUPXDGK8gDLAyo3q-MAdVxGY7vB3kV5efhXEqgP4urYCEb1cuarkgVw",
+            "wwrtx.ltype": "1",
+            "wxpay.corpid": "1970324966119483",
+            "wxpay.vid": "1688851857778143"
+        }
+        for k, v in cookies.items():
+            self.driver.add_cookie({"name": k, "value": v})
+        self.driver.get(url)
+        sleep(20)
+
+
+
+
